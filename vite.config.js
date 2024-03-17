1c8d19dbea5388b45623669a597090bd99270206
homeqwert <home1201@gmail.com>
2024-03-17 18:53:34 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    https: false, // HTTPS 비활성화
+    host: true, // 요청 수신 IP 설정
+    hmr: { host: 'localhost', protocol: 'ws' }, // HMR 엔더포인트 및 프로토콜 설정
+  },
+});
